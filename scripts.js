4ad210e1021a898e6a85c165979d392c367a6221
nancyepey <nancyepey97@gmail.com>
2022-06-23 10:57:46 +0100

    created basic arithmetic operations

---
+++ scripts.js
@@ -0,0 +1,34 @@
+console.log("start");
+
+// calculation with basic operations
+// addition, substraction, multiplication, division
+
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+
+function subst(num1, num2) {
+    const substraction_result = num1 - num2;
+    return substraction_result;
+}
+
+function multi(num1, num2) {
+    const milti_result = num1 * num2;
+    return milti_result;
+}
+
+function div(num1, num2) {
+    const division_result = num1 / num2;
+    return division_result;
+}
+
+
+console.log(add(2,3));
+console.log(subst(7,3));
+console.log(multi(2,3));
+console.log(div(9,3));
+
+// const input = prompt("What's your name? ");
+// console.log(input);
